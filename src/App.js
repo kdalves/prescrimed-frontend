d311191b0923c51b8f9c9f482ed98d6d47f549b7
Lucas Reis <37871167+lucasreisds@users.noreply.github.com>
2020-11-06 22:13:51 -0300

    fix: problemas de escrita no css  e diretorios de img (#9)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter} from 'react-router-dom';
 import Routes from './routes/routes';
+import "bootstrap/dist/css/bootstrap.min.css";
+import './styles/global.css'
 
 function App() {
   return (
