db984b4e7331c78e4c6441472f2491ca46e8c5ba
Kathleen (Kath) Alves <49501299+kdalves@users.noreply.github.com>
2020-11-23 21:49:23 -0300

    feat: New utils and new function formattedDate

--- src/components/patientList/patientTableColumn.js
+++ src/components/patientList/patientTableColumn.js
@@ -1,23 +1,17 @@
 import React from 'react';
-import { 
-    parseISO, 
-    format
-  } from 'date-fns';
-  
+import formattedDate from '../../utils/formattedDate';
 
-const PatientTableColumn = ({ patient }) => {
-
+const PatientTableColumn = ({ patient }) => {    
     return (
         <tr>
             <td> <label htmlFor="nome">{patient.Nome} </label></td>
             <td htmlFor="nome_social">{patient.NomeSocial}</td>
             <td htmlFor="cpf">{patient.CPF}</td>
             <td htmlFor="gender">{patient.Sexo}</td>
-            <td htmlFor="birth">{ format(parseISO(patient.Nascimento), 'DD/MM/YYYY')}</td>
+            <td htmlFor="birth">{formattedDate(patient.Nascimento)}</td>
             <th></th>
             <th></th>
         </tr>
     );
-
 }
 export default PatientTableColumn;
