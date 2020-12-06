d13fad100d80bfd456eca052228dedbbc77955a9
Lucas Reis <lucasrdsmarley@gmail.com>
2020-12-06 16:05:47 -0300

    add: populando selects e radios

--- src/pages/medics/MedicRegistrationNew.js
+++ src/pages/medics/MedicRegistrationNew.js
@@ -6,6 +6,7 @@ import GetIDProfissionais from "../../services/ServiceProfissionais/getIDProfiss
 import PostProfissionais from "../../services/ServiceProfissionais/postProfissionais";
 import PutProfissionais from "../../services/ServiceProfissionais/putProfissionais";
 import ContainerForm from "../../components/UI/container/containerForm";
+import { Multiselect } from 'multiselect-react-dropdown';
 
 const initialValue = {
   "IdProfissional": 0,
@@ -17,15 +18,18 @@ const initialValue = {
   "IdCategoria": 0,
   "IdSetor": 0
 }
+console.log(initialValue.IdCategoria);
 
 export default function MedicRegistrationNew({ id }) {
 
   const [values, setValues] = useState(initialValue);
+  console.log('values: ', values);
+
   const history = useHistory();
 
   const postProfissionais = async (valor) => {
     const response = await PostProfissionais(valor)
-    console.log(response);
+    console.log('post medicos: ', response);
   }
 
   const putProfissionais = async (id, valor) => {
@@ -49,6 +53,54 @@ export default function MedicRegistrationNew({ id }) {
     const { name, value } = event.target;
 
     setValues({ ...values, [name]: value });
+  }
+
+  function onChangeCategories(event) {
+    if (event.target.value === "medico") {
+      const valor = (values.IdCategoria = 1);
+      setValues({ ...values, valor });
+    } else if (event.target.value === "enfermeiro") {
+      const valor = (values.IdCategoria = 2);
+      setValues({ ...values, valor });
+    } else if (event.target.value === "farmaceutico") {
+      const valor = (values.IdCategoria = 3);
+      setValues({ ...values, valor });
+    } else if (event.target.value === "recepcionista") {
+      const valor = (values.IdCategoria = 4);
+      setValues({ ...values, valor });
+    }
+  }
+
+  function onChangeSpecialties(event) {
+    if (event.target.value === "cirurgiao") {
+      const valor = (values.IdEspecialidade = 1);
+      setValues({ ...values, valor });
+    } else if (event.target.value === "psiquiatra") {
+      const valor = (values.IdEspecialidade = 2);
+      setValues({ ...values, valor });
+    } else if (event.target.value === "dermatologista") {
+      const valor = (values.IdEspecialidade = 3);
+      setValues({ ...values, valor });
+    } else if (event.target.value === "pediatra") {
+      const valor = (values.IdEspecialidade = 4);
+      setValues({ ...values, valor });
+    }
+  }
+
+  function onChangeSectors(event) {
+    if (event.target.value === "centro-cirurgico") {
+      const valor = (values.IdSetor = 1);
+      setValues({ ...values, valor });
+    } else if (event.target.value === "psiquiatria") {
+      const valor = (values.IdSetor = 2);
+      setValues({ ...values, valor });
+    } else if (event.target.value === "oncologia") {
+      const valor = (values.IdSetor = 3);
+      setValues({ ...values, valor });
+    } else if (event.target.value === "pediatria") {
+      const valor = (values.IdSetor = 4);
+      setValues({ ...values, valor });
+    }
   }
 
   function onSubmit(event) {
@@ -103,31 +155,36 @@ export default function MedicRegistrationNew({ id }) {
 
               <div className="input-block">
                 <label htmlFor="medico_categoria" for="medico_categoria">Categoria</label>
-                <select htmlFor="medico_categoria" name="IdCategoria" id="IdCategoria" form="categoriaform">
-                  <option value={values?.IdCategoria === 1}>Médico</option>
-                  <option value={values?.IdCategoria === 2}>Enfermeiro</option>
-                  <option value={values?.IdCategoria === 3}>Farmacêutico</option>
-                  <option value={values?.IdCategoria === 4}>Recepcionista</option>
+                <select onChange={onChangeCategories} htmlFor="medico_categoria" name="IdCategoria" id="IdCategoria" form="categoriaform">
+                  <option value={"medico"}>Médico</option>
+                  <option value={"enfermeiro"}>Enfermeiro</option>
+                  <option value={"farmaceutico"}>Farmacêutico</option>
+                  <option value={"recepcionista"}>Recepcionista</option>
                 </select>
               </div>
 
               <div className="input-block">
                 <label htmlFor="medico_especialidade" for="medico_categoria">Especialidade</label>
-                <select htmlFor="medico_especialidade" name="IdEspecialidade" id="IdEspecialidade" form="especialidadeform">
-                  <option value={values?.IdEspecialidade === 1}>Cirurgião</option>
-                  <option value={values?.IdEspecialidade === 2}>Psiquiatra</option>
-                  <option value={values?.IdEspecialidade === 3}>Dermatologista</option>
-                  <option value={values?.IdEspecialidade === 4}>Pediatra</option>
+                <select onChange={onChangeSpecialties} htmlFor="medico_especialidade" name="IdEspecialidade" id="IdEspecialidade" form="especialidadeform">
+                  <option value={'cirurgiao'}>Cirurgião</option>
+                  <option value={'psiquiatra'}>Psiquiatra</option>
+                  <option value={'dermatologista'}>Dermatologista</option>
+                  <option value={'pediatra'}>Pediatra</option>
                 </select>
+                {/* <Multiselect
+                  options={objectArray}
+                  displayValue="key"
+                  singleSelect
+                /> */}
               </div>
 
               <div className="input-block">
                 <label htmlFor="medico_setor" for="medico_setor">Setor</label>
-                <select htmlFor="medico_setor" name="IdSetor" id="IdSetor" form="setorform">
-                  <option value={values?.IdSetor === 1}>Centro Cirurgico</option>
-                  <option value={values?.IdSetor === 2}>Psiquiatria</option>
-                  <option value={values?.IdSetor === 3}>Oncologia</option>
-                  <option value={values?.IdSetor === 4}>Pediatria</option>
+                <select onChange={onChangeSectors} htmlFor="medico_setor" name="IdSetor" id="IdSetor" form="setorform">
+                  <option value={'centro-cirurgico'}>Centro Cirurgico</option>
+                  <option value={'psiquiatria'}>Psiquiatria</option>
+                  <option value={'oncologia'}>Oncologia</option>
+                  <option value={'pediatria'}>Pediatria</option>
                 </select>
               </div>
 
