45a60c3910217f900f6058cd718749fd70c77e4e
Lucas Reis <lucasrdsmarley@gmail.com>
2020-12-07 17:00:02 -0300

    fix: tela inicial

--- src/pages/nav/Nav.js
+++ src/pages/nav/Nav.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import '../../styles/pages/nav/menu-nav.css'
 import Sidebar from '../../components/sidebar/Sidebar';
+import LogoImg from '../../images/prescrimedLogo2Grey.png';
 
 function NavBar() {
 
     return (
-        <div id="page-nav">       
-            <Sidebar/>
+        <div id="page-nav">
+            <Sidebar />
+            <div className="logo-content">
+                <img src={LogoImg} alt="Logo prescrimed"></img>
+            </div>
+
         </div>
     )
 }
