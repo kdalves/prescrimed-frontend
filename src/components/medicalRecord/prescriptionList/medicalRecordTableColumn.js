a208ba9c199b96a43515d46f6cbdb72611c81fd4
Kathleen (Kath) Alves <49501299+kdalves@users.noreply.github.com>
2020-12-07 19:18:40 -0300

    feat: Adjusted some stuff

--- src/components/medicalRecord/prescriptionList/medicalRecordTableColumn.js
+++ src/components/medicalRecord/prescriptionList/medicalRecordTableColumn.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ColumnUpdateRemove from '../../tableComponents/columnUpdateRemove';
 import Buttons from '../../editDelButton/EditDelButton';
 import formattedDate from '../../../utils/formattedDate';
+import { Link } from 'react-router-dom';
+import pdfIcon from '../../../images/pdf.png';
+// import deleteIcon from '../../../images/close.png';
+// import updateIcon from '../../../images/edit-notebook.png';
 
 const MedicalRecordColumn = ( { prescription }) => {
 
@@ -11,8 +15,13 @@ const MedicalRecordColumn = ( { prescription }) => {
             <td>{formattedDate(prescription.Data)}</td>
             <td>{prescription.IdProfissional}</td>
             <td>{prescription.IdProfissional}</td>
-            <td>{formattedDate(prescription.DataAssinatura)}</td>            
-            <td><Buttons routeEdit={`lala`} delete={`lala`}/></td>
+            <td>{formattedDate(prescription.DataAssinatura)}</td>
+            <td><Link to={`lala`}><img src={pdfIcon} width="25px" /></Link></td>
+            <td>
+                {/* <Link to={`lala`}><img src={updateIcon} width="25px" /></Link>
+                <button style={{ border: 0, background: 0 }}><img  src={deleteIcon}  width="20px" /></button> */}
+                <Buttons routeEdit={`lala`} delete={`lala`}/>
+            </td>
             <ColumnUpdateRemove />
         </tr>
     )
--- src/components/prescriptionDetailsList/prescriptionDetailsListComp.js
+++ src/components/prescriptionDetailsList/prescriptionDetailsListComp.js
@@ -2,23 +2,29 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Table } from 'react-bootstrap';
 import PrescriptionDetailsTableColumn from './prescriptionDetailsTableColumn';
+import AddButton from '../../components/addButton/addButton';
 
 const PrescriptionDetailsList = ({ prescriptionDetails }) => (
-    <Table striped bordered hover>
-        <thead>
-            <tr>
-                <th>Medicamento</th>
-                <th>Dosagem</th>
-                <th>Unidade de Medida </th>
-                <th>Posologia</th>
-            </tr>
-        </thead>
-        <tbody>
-            {/* {prescriptionDetails.map((details) => (
-                <PrescriptionDetailsTableColumn prescriptionDetail={prescriptionDetails} />
-            ))} */}
-        </tbody>
-    </Table>
+    <>
+        <div className="plus-content">
+            <AddButton text="Adicionar Medicamentos " route="cadastroProntuario" />
+        </div>
+        <Table striped bordered hover>
+            <thead>
+                <tr>
+                    <th>Medicamento</th>
+                    <th>Dosagem</th>
+                    <th>Unidade de Medida </th>
+                    <th>Posologia</th>
+                </tr>
+            </thead>
+            <tbody>
+                {/* {prescriptionDetails.map((details) => (
+                    <PrescriptionDetailsTableColumn prescriptionDetail={prescriptionDetails} />
+                ))} */}
+            </tbody>
+        </Table>
+    </>
 );
 
 export default PrescriptionDetailsList;
